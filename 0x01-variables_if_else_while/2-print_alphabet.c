c209ce780b4ebf57c43746c407f5972d3a85a0d0
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-17 00:03:12 -0700

    Today's coding is crazy

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+
+{
+	char letter;
+	
+	for (letter = a; latter <= z; letter++)
+	{
+		putchar(letter);
+	}
+	putchar("\n");
+
+	return (0);
+}

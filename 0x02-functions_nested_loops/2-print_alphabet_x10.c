ff0101ef3b0f68524325c29b5fbb51603b293f92
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-20 14:22:46 -0700

    My first C nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char alp;
+
+	{
+		for (i = 0; i < 10; i++)
+		{
+			for (alp = 'a'; alp <= 'z'; alp++)
+			{
+				_putchar(alp);
+			}
+		}
+	}
+	_putchar('\n');
+}

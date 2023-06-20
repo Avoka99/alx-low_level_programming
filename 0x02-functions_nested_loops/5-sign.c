4959e4e741687940db9b428f914625c21b05977d
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-20 16:41:47 -0700

    My first C nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,14 +4,14 @@
  * print_sign - prints the sign of a number
  * @n: the number to check
  *
- * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		putchar('+');
-		return(1);
+		return (1);
 	}
 
 	else if (n == 0)

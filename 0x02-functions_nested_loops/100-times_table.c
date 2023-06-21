3817cee027afa6c4e672de5c277104e0d5f0c4d7
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-21 05:38:29 -0700

    My first C nested loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,13 +21,13 @@ void print_times_table(int n)
 
 
 			if (b == 0)
-			{
+
 				printf("%d", result);
-			}
+
 			else
-			{
-				printf(",%3d", result);
-			}
+
+				printf(", %d", result);
+
 		}
 		printf("\n");
 	}

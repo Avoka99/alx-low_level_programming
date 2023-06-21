8f06cfc865fb9f20711fd4c5596759ec2bdb3d74
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-20 18:43:45 -0700

    My first C nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			int result = a * b;
+			printf("%2d ", result);
+		}
+		printf("\n");
+	}
+}

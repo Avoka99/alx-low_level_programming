18198fcf66cf1c54e01ccd308db26eab74a63aee
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-20 17:19:50 -0700

    My first C nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the integer
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+
+}

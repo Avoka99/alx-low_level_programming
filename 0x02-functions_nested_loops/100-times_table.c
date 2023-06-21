976716a5fafe345d9320666884d5556fa3ffd821
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-21 05:08:08 -0700

    My first C nested loop

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: the number of times
+ */
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+
+	int a, b;
+
+	for (a = 0; a <= 15; a++)
+	{
+		for (b = 0; b <= 15; b++)
+		{
+			int result = a * b;
+
+
+			if (b == 0)
+			{
+				printf("%2d", result);
+			}
+			else
+			{
+				printf(", %2d", result);
+			}
+		}
+		printf("\n");
+	}
+}

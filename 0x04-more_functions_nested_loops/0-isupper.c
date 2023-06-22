28a9ffa49f7d7d6de978ee3e537ac64c68990c2f
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-22 06:40:05 -0700

    C functions are best of coding

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,7 @@
  * _isupper - checks if letter is in uppercase
  * @c: character to be check
  *
- * return: 1 if c is upper, 0 otherwise
+ * Return: 1 if c is upper, 0 otherwise
  */
 int _isupper(int c)
 {

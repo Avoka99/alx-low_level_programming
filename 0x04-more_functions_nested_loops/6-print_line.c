0dee3b69c2ecc88a9d7a9e3bf8e7a16411feddac
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-22 13:07:06 -0700

    C functions are the best of coding

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,7 +7,6 @@
  */
 void print_line(int n)
 {
-	int i;
 
 	if (n <= 0)
 	{
@@ -15,9 +14,10 @@ void print_line(int n)
 		return;
 	}
 
-	for (i = 0; i <= n; i++)
+	for (n > 0)
 	{
 		_putchar('_');
+		n--;
 	}
 	_putchar('\n');
 }

4168684b1a3d5091138629f142bc4889a1cccd23
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-22 07:58:14 -0700

    C functions are best of coding

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,12 @@
 void print_numbers(void)
 {
 	int num;
+	char digit = "0123456789";
 
 	for (num = 0; num <= 9; num++)
 	{
 
-		_putchar(num + '0 ');
+		_putchar(digit[num]);
 
 	}
 	_putchar('\n');

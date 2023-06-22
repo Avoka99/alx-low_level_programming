260b3041b6543cccc12e3324ee1f8b523c58d50e
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-22 07:14:43 -0700

    C functions are best of coding

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+
+		_putchar("%d ", num);
+
+	}
+	_putchar('\n');
+
+}

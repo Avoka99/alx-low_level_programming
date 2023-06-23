fdfb231eaac29297324bd27728641d00bf258e17
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-22 17:54:13 -0700

    C functions are best of coding

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long number = 612852475143;
+	long largest = 0;
+	long factor = 2;
+
+	while (number % 2 == 0)
+	{
+		largest = 2;
+		number /= 2;
+	}
+
+	while (factor * factor <= number)
+	{
+		if (number % factor == 0)
+		{
+			largest = factor;
+			number /= factor;
+		}
+		else
+		{
+			factor += 2;
+		}
+	}
+
+	if (number > 1)
+		largest = number;
+
+	printf("%ld\n", largest);
+
+	return 0;
+}

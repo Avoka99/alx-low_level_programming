66cdd04b56f7cee7a1f76618cf88a64d6d018da0
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-22 22:29:36 -0700

    C functions

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,8 +17,15 @@ void print_triangle(int size)
 	}
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j <= i; j++)
+		for (j = 1; j <= size; j++)
 		{
+
+			_putchar(' ');
+		}
+
+		for (j = 1; j <= i j++)
+		{
+
 			_putchar('#');
 		}
 		_putchar('\n');

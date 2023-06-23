ce2e2708b42f4ae004e723c0fc00294dc927e3df
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-22 22:51:34 -0700

    C functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,21 +7,20 @@
  */
 void print_number(int n)
 {
-	unsigned int i;
 
-	if (i < 0)
+	if (n < 0)
 	{
 		_putchar('-');
-		i = -n;
+		n = -n;
 	}
 	else
 	{
-		i = n;
+		n = n;
 	}
 
-	if (i / 10)
+	if (n / 10)
 
-		print_number(i / 10);
+		print_number(n / 10);
 
-	_putchar(i % 10 + '0');
+	_putchar(n % 10 + '0');
 }

860303070a3aa4bf8ef131d1614dbfd9bad8fca3
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-26 15:10:11 -0700

    learning pointers is easy

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: The value of the string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	if (str == 0)
+		return;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}

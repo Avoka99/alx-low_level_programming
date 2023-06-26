1f4369194ee56006cd62a513db876e3b0311f59e
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-26 12:39:36 -0700

    learning pointers is easy

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - calculate the length of a string
+ * @s: The input string
+ *
+ * Return: The length the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	while (str[len] != '\n')
+	{
+		len++;
+	}
+	return (len);
+}

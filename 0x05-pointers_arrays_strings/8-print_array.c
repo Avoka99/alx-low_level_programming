42ae8311cec177f2e155086c7e3d7e3fa6ac8185
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-26 16:04:12 -0700

    learning pointers is easy

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints arrays in n numbers
+ * @n: the number of array
+ * @a: the input array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (a == NULL || n <= 0)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		
+		if (i < n -1)
+			printf (", ");
+	}
+	printf("\n");
+}

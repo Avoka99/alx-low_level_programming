eaf63f31ca3e66e79c1d1863bef2714aa9315a4f
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-26 14:40:43 -0700

    learning pointers is easy

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ * @s: value of string
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int start = 0;
+	int end = len - 1;
+	char temp;
+
+	if (s == NULL)
+		return;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
+		start++;
+		end--;
+	}
+}

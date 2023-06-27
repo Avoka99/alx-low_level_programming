0a7f65447b6b92a49b15d1c5e21d40af66ff900e
Avoka99 <benjaminlahadi99@gmail.com>
2023-06-26 18:51:34 -0700

    learning pointers is easy

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,37 +9,27 @@
  */
 int _atoi(char *s)
 {
-	int r = 0;
+	int result = 0;
 	int sign = 1;
 	int i = 0;
 
-	while (s[i] != '\0')
+	if (s[0] == '-')
 	{
-		if (s[i] == ' ')
-		{
-			i++;
-			continue;
-		}
-		if (s[i] == '-')
-		{
-			sign = -1;
-			i++;
-		}
-		else if (s[i] == '+')
-		{
-			i++;
-		}
-
-		while (isdigit(s[i]))
-		{	
-			r = r * 10 + (s[i] - '0');
-			i++;
-		
-		}
-		
-		r *= sign;
-
-		break;
+		sign = -1;
+		i++;
 	}
-	return (r);
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (result != 0)
+		{
+			break;
+		}
+	}
+
+	return result * sign;
 }

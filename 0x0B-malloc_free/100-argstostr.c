62bbd9063098e4521850e0aa6867f2fbaec13518
Avoka99 <benjaminlahadi99@gmail.com>
2023-07-10 20:14:31 -0700

    Mallocs are fun

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * argstostr -  concatenates all the arguments
+ * @ac: input value
+ * @av: double pointer
+ *
+ * Return: pointer to a new string or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int t_len, n_len, f_len, p = 0;
+	char *results;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	t_len = 0;
+	for (i = 0; i < ac; i++)
+		t_len += strlen(av[i]);
+
+	n_len = ac - 1;
+	t_len += n_len;
+	f_len = t_len + 1;
+
+	results = malloc(f_len);
+	if (results == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(results + p, av[i]);
+		p += strlen(av[i]);
+
+		if (i != ac - 1)
+			results[p++] = '\n';
+	}
+	results[p] = '\0';
+	return (results);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,139 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: string value
+ *
+ * Return: a pointer to an array of strings(words)
+ */
+
+int count_words(char *str);
+char *extract_word(char *str);
+void free_words(char **words);
+
+char **strtow(char *str)
+{
+	int num_words;
+	int index = 0;
+	char **words, *word;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	num_words = count_words(str);
+	if (num_words == 0)
+		return (NULL);
+
+	words = malloc((num_words + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+
+
+	word = extract_word(str);
+	while (word != NULL)
+	{
+		words[index++] = word;
+		word = extract_word(NULL);
+	}
+
+	words[index] = NULL;
+
+	return (words);
+}
+
+/**
+ * count_words - counts words in the string pointer
+ * @str: value of string pointer
+ *
+ * Return: number of words
+ */
+
+int count_words(char *str)
+{
+	int count = 0;
+	int length = strlen(str);
+	int i = 0;
+
+	while (i < length)
+	{
+		while (str[i] == ' ')
+			i++;
+
+		if (i == length)
+			break;
+
+		count++;
+
+		while (str[i] != ' ' && str[i] != '\0')
+			i++;
+	}
+
+	return (count);
+}
+
+/**
+ * extract_word - remove words from the string pointer
+ * @str: value of string pointer
+ *
+ * Return: word
+ */
+
+char *extract_word(char *str)
+{
+	static char *current_str;
+	static char *next_str;
+	int word_length;
+	char *word;
+
+	if (str != NULL)
+	{
+		current_str = str;
+		next_str = str;
+	}
+
+	while (*next_str == ' ')
+		next_str++;
+
+	if (*next_str == '\0')
+		return (NULL);
+
+	current_str = next_str;
+
+	while (*next_str != ' ' && *next_str != '\0')
+		next_str++;
+
+	word_length = next_str - current_str;
+	word = malloc((word_length + 1) * sizeof(char));
+
+	if (word != NULL)
+	{
+		strncpy(word, current_str, word_length);
+		word[word_length] = '\0';
+	}
+
+	return (word);
+}
+
+/**
+ * free_words - free the memory allocated by strtow
+ * @words: double pointer
+ *
+ * Return: free
+ */
+
+void free_words(char **words)
+{
+	if (words == NULL)
+		return;
+
+	index = 0;
+	while (words[index] != NULL)
+	{
+		free(words[index]);
+		index++;
+	}
+
+	free(words);
+}

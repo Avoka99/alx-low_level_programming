1d47880e1df88ba42caecbffc1fa9c6502ee549c
Avoka99 <benjaminlahadi99@gmail.com>
2023-07-17 14:05:57 -0700

    Hey, I made structures and they struct me down

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints details of a dog
+ * @d: pointer to struct dog
+ *
+ * Return: Always 0
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: %s\n", d->name != NULL ? d->name: "(nil)");
+	printf("Age: %.lf\n", d->age);
+	printf("Owner: %s\n", d->owner != NULL ? d->owner: "(nil)");
+}

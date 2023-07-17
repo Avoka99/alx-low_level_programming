54889bec81edb368e849f45862fad6b5992e4066
Avoka99 <benjaminlahadi99@gmail.com>
2023-07-17 16:17:04 -0700

    Hey, I made structures and they struct me down

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,25 +16,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	char *name_copy;
 	char *owner_copy;
+	int name_len, owner_len;
+
+	name_len = strlen(name);
+	owner_len = strlen(owner);
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	name_copy = strdup(name);
+	name_copy = malloc(sizeof(char) * (name_len + 1));
 	if (name_copy == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
+	strcpy(name_copy, name);
 
-	owner_copy = strdup(owner);
+	owner_copy = malloc(sizeof(char) * (owner_len + 1));
 	if (owner_copy == NULL)
 	{
 		free(new_dog);
 		free(name_copy);
 		return (NULL);
 	}
+	strcpy(owner_copy, owner);
 
 	new_dog->name = name_copy;
 	new_dog->age = age;

5b3c0c3daad0df8d0678260179e09da354f9cdd8
Avoka99 <benjaminlahadi99@gmail.com>
2023-07-17 09:31:39 -0700

    Hey Lizy, you are getting married to man a who got married to his computer?

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *f_name = __FILE__;
+	int i = 0;
+
+	while (f_name[i] != '\0')
+	{
+		_putchar(f_name[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}

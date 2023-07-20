58c71be041f7847a8f60ee7b896da07b23f24020
Avoka99 <benjaminlahadi99@gmail.com>
2023-07-20 16:21:17 -0700

    committing, but the commitment is less

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,47 +9,43 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	char *str;
 	int i = 0;
-	char c;
+	char *str, *sep = "";
 
-	va_start(args, format);
+	va_list list;
 
-	while (format && format[i])
+	va_start(list, format);
+
+	if (format)
 	{
-		switch (format[i])
+		while (format[i])
 		{
-			case 'c':
-				c = va_arg(args, int);
-				printf("%c", c);
-				break;
-
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s", str);
-				break;
-
-			default:
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
 		}
-
-		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
-			printf(", ");
-
-		i++;
 	}
 
-	va_end(args);
 	printf("\n");
+	va_end(list);
 }

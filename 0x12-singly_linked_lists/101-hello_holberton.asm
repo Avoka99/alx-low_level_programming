c5cce8d078e84507164ea28a7e185f7e09a3d8d1
Avoka99 <benjaminlahadi99@gmail.com>
2023-07-27 19:20:24 -0700

    Hey lists my names in order

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,25 +1,10 @@
-section .data
-hello db 'Hello, Holberton', 0
-format db '%s', 10, 0
+global   main
+	  extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
-section .text
-global _start
-
-_start:
-; Prepare arguments for printf
-mov rdi, format
-mov rsi, hello
-xor rax, rax
-
-; Call printf
-call printf
-
-; Exit the program
-mov rdi, 0
-call exit
-
-section .bss
-resb 1
-
-extern printf
-extern exit

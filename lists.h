176157fa71f097cab35494ba5a7c5ffb237e5ffd
Avoka99 <benjaminlahadi99@gmail.com>
2023-07-27 17:11:11 -0700

    Hey lists my names in order

--- lists.h
+++ lists.h
@@ -1,8 +1,22 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
+/* Function prototype for print_list */
 void free_list(list_t *head);
 
 list_t *add_node_end(list_t **head, const char *str);
@@ -15,4 +29,4 @@ size_t print_list(const list_t *h);
 
 int _putchar(char c);
 
-#endif
+#endif /* LISTS_H */

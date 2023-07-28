5c3cf2a7718afb28a682f42e11bf532e8386fd0b
Avoka99 <benjaminlahadi99@gmail.com>
2023-07-27 18:17:48 -0700

    Hey lists my names in order

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -25,7 +25,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return( NULL);
+		return (NULL);
 	}
 
 	new_node->len = strlen(str);
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a linked list.
+ * @head: Pointer to the head of the list.
+ */
+void free_list(list_t *head)
+{
+	list_t *current, *next_node;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
+	}
+}

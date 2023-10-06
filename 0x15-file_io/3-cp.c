96e7421bcd443cbafbd10a4b565b46ed21540c37
Avoka99 <benjaminlahadi99@gmail.com>
2023-10-06 12:36:34 -0700

    file handling operation

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,66 +4,128 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 1024
+char *create_buffer(const char *file);
+void close_file(int fd);
 
-/*
- * main - Copy the content of one file to another.
- * @argc: The number of arguments.
- * @argv: An array of argument strings.
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
  *
- * Return: 0 on success, or an appropriate exit code on failure.
+ * Return: A pointer to the newly-allocated buffer.
+ */
+char *create_buffer(const char *file)
+{
+	int fd;
+	char *buffer;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		return (NULL);
+	}
+
+	buffer = (char *)malloc(BUFFER_SIZE);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Memory allocation failed.\n");
+		close(fd);
+		return (NULL);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (errno == EBADF ? -1 : errno));
+	}
+}
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, wr_result;
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read;
+	const char *source_filename = argv[1];
+	const char *destination_filename = argv[2];
+
+	char *buffer;
+	int fd_to, fd_from;
+	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+
+	buffer = create_buffer(source_filename);
+	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		return (98);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(destination_filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", destination_filename);
+		close_file(fd_to);
+		free(buffer);
+		return (99);
 	}
+
+	dprintf(STDOUT_FILENO, "Copying from %s to %s...\n", source_filename, destination_filename);
+
+	fd_from = open(source_filename, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source_filename);
+		close_file(fd_to);
+		free(buffer);
+		return (98);
+	}
+
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		wr_result = write(fd_to, buffer, bytes_read);
-		if (wr_result == -1)
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", destination_filename);
+			close_file(fd_from);
+			close_file(fd_to);
+			free(buffer);
+			return (99);
 		}
 	}
 
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source_filename);
+		close_file(fd_from);
+		close_file(fd_to);
+		free(buffer);
+		return (98);
 	}
 
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-		exit(100);
-	}
+	close_file(fd_from);
+	close_file(fd_to);
+	free(buffer);
+
+	dprintf(STDOUT_FILENO, "Copy completed successfully.\n");
 
 	return (0);
 }
+

5b274f3aa5de0b2c7fbc586d0d1b8517d3f76942
Avoka99 <benjaminlahadi99@gmail.com>
2023-10-30 05:47:36 -0700

    files handling management

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,6 +26,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
+			;
 
 		result = write(fd, text_content, i);
 		if (result == -1)
